2bd18dab41b6ba4f4c4cb00bc2a39774f9b10c44
nico359 <124350776+nico359@users.noreply.github.com>
2026-05-09 17:16:03 +0200

    Delete run.sh
    Deleted it because I only use Gnome Builder anyway

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Quick dev build+run script (not used by meson/flatpak)
-set -e
-cd "$(dirname "$0")"
-glib-compile-resources src/loci.gresource.xml --target=loci.gresource
-cargo build
-GSETTINGS_SCHEMA_DIR=data ./target/debug/loci
