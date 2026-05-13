12ee4709fb7c19aa9a483341e9a680e2e03af867
loci <124350776+nico359@users.noreply.github.com>
2026-05-13 10:46:41 +0200

    Bumped version number
    Bumped version number and added release to metainfo

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-pub static VERSION: &str = "0.1.0";
+pub static VERSION: &str = "0.1.1";
 pub static GETTEXT_PACKAGE: &str = "loci";
 pub static LOCALEDIR: &str = "/app/share/locale";
 pub static PKGDATADIR: &str = "/app/share/loci";
